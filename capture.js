71176dead6b430033ccf262cd399a58726859c3a
Chris Price <price.c@gmail.com>
2016-02-22 11:26:13 +0000

    Render to gif

--- capture.js
+++ capture.js
@@ -1,28 +1,50 @@
 "use strict";
 
-var url = 'https://t.d3fc.io/status/700046974305857536',
-  viewport = '1024,768'.split(',');
-
+var zpad = require('zpad');
+var args = require('system').args;
 var page = require('webpage').create();
-page.viewportSize = { width: viewport[0], height : viewport[1] };
-page.open(url, function(status) {
-  if (status === 'success') {
-    var bbox = page.evaluate(function() {
-      var elem = document.querySelector('iframe');
-      return elem && elem.getBoundingClientRect();
-    });
-    if (bbox) {
-      page.clipRect = bbox;
-      var frame = 0, frameCount = 10;
-      setInterval(function() {
-        if (frame === frameCount) {
-          return phantom.exit();
-        }
-        page.render('frame-' + frame + '.png');
-        frame++;
-      }, 16);
-    }
+
+if (args.length !== 2) {
+  console.error('Not enough arguments');
+  phantom.exit(1);
+}
+
+var options = JSON.parse(args[1]);
+
+function getBBox(selector) {
+  var elem = document.querySelector(selector);
+  return elem && elem.getBoundingClientRect();
+}
+
+function pageLoaded(status) {
+  if (status !== 'success') {
+    console.error('URL failed to load');
+    return phantom.exit(1);
   }
-})
-// // page.evaluate(function() {
-// // });
+  var bbox = page.evaluate(getBBox, options.selector);
+  if (!bbox) {
+    console.error('Element not found');
+    return phantom.exit(1);
+  }
+  if (bbox.width * bbox.height > options.maxArea) {
+    console.error('BBox too large');
+    return phantom.exit(1);
+  }
+  console.log(JSON.stringify(bbox));
+  page.clipRect = bbox;
+  var frame = 0;
+  setInterval(function() {
+    if (frame >= options.frameCount) {
+      return phantom.exit();
+    }
+    var filename = options.pattern.replace('$i', zpad(frame, 5));
+    page.render(filename);
+    frame++;
+  }, options.frameInterval);
+}
+
+page.viewportSize = {
+  width: options.viewport[0],
+  height : options.viewport[1]
+};
+page.open(options.url, pageLoaded);
--- index.js
+++ index.js
@@ -1,19 +1,105 @@
 "use strict";
 
-const path = require('path')
+const winston = require('winston');
+const express = require('express');
+const rimraf = require('rimraf');
+const path = require('path');
+const uuid = require('uuid').v4;
 const execFile = require('child_process').execFile;
+const phantomjs = require('phantomjs-prebuilt/lib/phantomjs');
 
-const binPath = require('phantomjs-prebuilt/lib/phantomjs').path;
+var GIFEncoder = require('gifencoder');
+var pngFileStream = require('png-file-stream');
 
-execFile(
-  binPath,
-  ['capture.js'],
-  {
-    cwd: __dirname,
-    timeout: 10000
-  },
-  (err, stdout, stderr) => {
-    console.log('error', err);
-    console.log(stdout);
-    console.error(stderr);
-  })
+const app = express();
+
+app.use((req, res, next) => {
+  winston.info(req.method, req.url, req.ip);
+  next();
+});
+
+// WARNING - These are way too high for exposing to the internet.
+// WARNING - Additionally there's no throttling of requests.
+// WARNING - Use at your own risk!
+const MAX_VIEWPORT = 1024 * 768;
+const MAX_AREA = 500 * 500;
+const MAX_FRAMES = 50;
+const MAX_DURATION = 5000;
+const MAX_LOADING = 5000;
+
+const workingDirectory = path.join(__dirname, 'frames');
+
+app.get('/', (req, res) => {
+  const options = Object.assign({}, {
+    url: 'about:blank',
+    selector: 'body',
+    viewport: '1024,768',
+    frameInterval: '100',
+    frameCount: '50',
+    repeat: '0',
+    quality: '10'
+  }, req.query);
+
+  options.viewport = options.viewport.split(',').map((v) => Number(v));
+  if (options.viewport.width * options.viewport.height > MAX_VIEWPORT) {
+    winston.warn('Max viewport exceeded');
+    return res.status(500).send();
+  }
+
+  options.frameCount = Number(options.frameCount);
+  if (isNaN(options.frameCount) || options.frameCount > MAX_FRAMES) {
+    winston.warn('Max frames exceeded');
+    return res.status(500).send();
+  }
+
+  options.frameInterval = Number(options.frameInterval);
+  if (isNaN(options.frameInterval) ||
+      options.frameCount * options.frameInterval > MAX_DURATION) {
+    winston.warn('Max duration exceeded');
+    return res.status(500).send();
+  }
+
+  const id = uuid();
+
+  execFile(
+    phantomjs.path,
+    [
+      'capture.js',
+      JSON.stringify(Object.assign({}, options, {
+        maxArea: MAX_AREA,
+        pattern: path.join('frames', `frame-${id}-$i.png`)
+      }))
+    ],
+    {
+      cwd: __dirname,
+      timeout: MAX_LOADING + MAX_DURATION
+    },
+    (err, stdout, stderr) => {
+      if (err) {
+        winston.warn(err, stdout, stderr);
+        return res.status(500).send();
+      }
+
+      var bBox = JSON.parse(stdout);
+
+      var encoder = new GIFEncoder(bBox.width, bBox.height)
+        .createWriteStream({
+          repeat: options.repeat,
+          delay: options.frameInterval,
+          quality: options.quality
+        });
+
+      res.writeHead(200, { "Content-Type": "image/gif" });
+
+      pngFileStream(path.join(workingDirectory, `frame-${id}-*.png`))
+        .pipe(encoder)
+        .pipe(res);
+
+      rimraf(path.join(workingDirectory, `frame-${id}-*.png`), (err) => {
+        winston.error(err, 'Could not delete files', id);
+      });
+    }
+  );
+});
+
+app.listen(3000);
